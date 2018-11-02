74e6448e2ebd578c4a4fd9637d6fe87e5925060e
shiva768 <reika.github@zelretch.com>
2018-11-02 20:23:19 +0900

    整形

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from audio_metadata import MP3
 from audio_metadata.formats.tables import ID3PictureType
 from google_music import MusicManager
+
 CREDENTIAL = './credential'
 
 
-
-org = MP3.load
-def load(data):
-    r = org(data)
-    if len(r.pictures) > 0 and r.pictures[0].type == ID3PictureType.OTHER:
-        r.pictures[0].type = ID3PictureType.COVER_FRONT
-    return r
-
-MP3.load = load
-
 def main():
+    monkeypatch()
     manager = MusicManager()
-    manager.upload('/home/shibata/downloads/130cm - せせりのテーマ.mp3')
+    manager.upload('path')
+
+
+def monkeypatch():
+    org = MP3.load
+
+    def load(data):
+        r = org(data)
+        if len(r.pictures) > 0 and r.pictures[0].type == ID3PictureType.OTHER:
+            r.pictures[0].type = ID3PictureType.COVER_FRONT
+        return r
+
+    MP3.load = load
 
 
 if __name__ == '__main__':
